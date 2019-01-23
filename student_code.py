c9cbba609080d6caa35bd7935ed7dfd8b9006d75
juztinfied <z_d_143@hotmail.com>
2019-01-23 09:46:55 -0600

    pass 5 tests

--- student_code.py
+++ student_code.py
@@ -18,14 +18,71 @@ class KnowledgeBase(object):
         return string
 
     def kb_assert(self, fact):
+        self.facts.append(fact)
+
         """Assert a fact or rule into the KB
 
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
+
+
         print("Asserting {!r}".format(fact))
         
     def kb_ask(self, fact):
+        q = fact
+        q_statement = fact.statement
+        response = ListOfBindings()
+        
+        if (not is_var(q_statement.terms[0]) and not is_var(q_statement.terms[1])):
+            for f in self.facts:
+                if (match(q_statement, f.statement)):
+                    newbindings = match(q_statement, f.statement)
+                    response.add_bindings(newbindings, f)
+
+            if response.__len__() == 0:
+                return False
+            
+            else:
+                return response
+        
+        elif (not is_var(q_statement.terms[0]) and is_var(q_statement.terms[1])):# if question is like color bigbox ?y
+            for f in self.facts:
+                if q_statement.predicate == f.statement.predicate and q_statement.terms[0].__eq__(f.statement.terms[0]):
+                    newbindings = match(q_statement, f.statement)
+                    response.add_bindings(newbindings, f)
+
+            if response.__len__() == 0:
+                return False 
+            
+            else:
+                return response
+        
+        elif  (is_var(q_statement.terms[0]) and not is_var(q_statement.terms[1])):
+            for f in self.facts:
+                if q_statement.predicate == f.statement.predicate and q_statement.terms[1].__eq__(f.statement.terms[1]):
+                    newbindings = match(q_statement, f.statement)
+                    response.add_bindings(newbindings, f)
+
+            if response.__len__() == 0:
+                return False 
+            
+            else:
+                return response
+
+
+        elif  (is_var(q_statement.terms[0]) and is_var(q_statement.terms[1])):
+            for f in self.facts:
+                if q_statement.predicate == f.statement.predicate:
+                    newbindings = match(q_statement, f.statement)
+                    response.add_bindings(newbindings, f)
+
+            if response.__len__() == 0:
+                return False 
+            
+            else:
+                return response
+
         """Ask if a fact is in the KB
 
         Args:
